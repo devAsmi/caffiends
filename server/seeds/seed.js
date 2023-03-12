e89a777906cae0f9224c05d994bdda6e497dbec7
devasmi <impokhrelasmita5@gmail.com>
2023-03-12 11:40:57 -0400

    added colditems seed and hot items seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,7 +1,9 @@
 const db = require("../config/connection");
 
 const { Item, ItemType } = require("../models");
+const createColdBeverage = require("./ColdItems");
 const createFoodItems = require("./FoodItems");
+const createHotBeverage = require("./HotItems");
 
 db.once("open", async () => {
   await ItemType.deleteMany();
@@ -14,33 +16,8 @@ db.once("open", async () => {
 
   await Item.deleteMany();
   const Items = await Item.insertMany([
-    {
-      name: "Hot coffee",
-      description: "A piping cup of hot coffee",
-      image:
-        "https://images.unsplash.com/photo-1517256064527-09c73fc73e38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      price: 2.99,
-      points: 200,
-      itemType: ItemTypes[0]._id,
-    },
-    {
-      name: "Hot chocolate",
-      description: "A piping cup of hot chocolate",
-      image:
-        "https://images.unsplash.com/photo-1608735484559-50aa06c9478c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      price: 3.99,
-      points: 300,
-      itemType: ItemTypes[0]._id,
-    },
-    {
-      name: "Cold coffee",
-      description: "A refreshing cup of iced cold coffee",
-      image:
-        "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
-      price: 2.99,
-      points: 200,
-      itemType: ItemTypes[1]._id,
-    },
+    ...createHotBeverage(ItemTypes[0]._id),
+    ...createColdBeverage(ItemTypes[1]._id),
     ...createFoodItems(ItemTypes[2]._id),
   ]);
   console.log("Items seeded");
