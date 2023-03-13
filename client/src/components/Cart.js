2d55494de61b95626fbbd05099831d5cf5c95332
devasmi <impokhrelasmita5@gmail.com>
2023-03-13 13:20:11 -0400

    added initial cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,4 +1,4 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useCartItemContext } from "../utils/GlobalState";
 import CartItem from "./CartItem";
@@ -6,15 +6,19 @@ import CartItem from "./CartItem";
 export default function Cart() {
   const [state, dispatch] = useCartItemContext();
   const { cartItems } = state;
+
   const [cart, setCart] = useState({});
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalPoint,setTotalPoint] = useState(0);
 
   useEffect(() => {
-    const cart = transformCartItems();
-    setCart(cart);
+    processCartItems();
   }, [cartItems]);
 
-  function transformCartItems() {
+  function processCartItems() {
     const cart = {};
+    let totalPriceForItems = 0;
+    let totalPointFotItems = 0;
     for (let i = 0; i < cartItems.length; i++) {
       const cartItem = cartItems[i];
       if (cartItem._id in cart) {
@@ -27,16 +31,28 @@ export default function Cart() {
           quantity: 1,
         };
       }
+      totalPriceForItems = cartItem.price + totalPriceForItems;
+      totalPointFotItems = cartItem.points + totalPointFotItems;
     }
-    return cart;
+    setTotalPrice(totalPriceForItems);
+    setTotalPoint(totalPointFotItems);
+    setCart(cart);
   }
 
   return (
     <Flex direction="column">
       <Heading size={"xl"}>Cart</Heading>
-      {Object.values(cart).map((item) => {
-        return <CartItem key={item.name} item={item} />;
-      })}
+      <UnorderedList>
+        {Object.values(cart).map((item) => {
+          return (
+            <ListItem key={item.name}>
+              <CartItem item={item} />
+            </ListItem>
+          );
+        })}
+      </UnorderedList>
+      <div>Total price: {totalPrice.toFixed(2)}</div>
+      <div>Total eligible points:{totalPoint} </div>
     </Flex>
   );
 }
--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,10 +1,13 @@
+import { Box, HStack } from "@chakra-ui/react";
 import React from "react";
 
 export default function CartItem({ item }) {
   return (
-    <div>
-      <p>{item.name}</p>
-      <p>{item.quantity}</p>
-    </div>
+    <HStack spacing="8">
+      <Box w="200px">{item.name}</Box>
+      <Box w="40px">{item.quantity}</Box>
+      <Box w="40px">{item.price}</Box>
+      <Box w="40px">{item.points}</Box>
+    </HStack>
   );
 }
